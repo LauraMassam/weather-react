84f8110211c12879b141103ab0859309d96793e4
LauraMassam <68440423+LauraMassam@users.noreply.github.com>
2021-06-27 19:15:31 +0100

    added footer (coded by)

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <Weather />
+  
+    <footer className="footer">
+       <br/>
+       Open-source code by {""}
+        <a href = "https://github.com/LauraMassam/weather-react" rel = "noreferrer" target="_blank">
+           Laura Massam          
+        </a>
+    </footer>
       </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -42,3 +42,6 @@ export default function Weather() {
     </form>
   );
 }
+  <footer>
+      coded by Laura Massam
+  </footer>
